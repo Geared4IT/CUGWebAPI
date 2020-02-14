a210ef69f40bfb682d99f98081ea9de8fc9c1af9
Jacques <Jacques@DESKTOP-JD4UDGG>
2020-02-14 02:25:15 +0200

    advanced search

--- cugonlineWebAPI/Controllers/WikiController.cs
+++ cugonlineWebAPI/Controllers/WikiController.cs
@@ -24,20 +24,79 @@ namespace cugonlineWebAPI.Controllers
         /// <returns></returns>
         [Route("GetSearchResultsBy")]
         [HttpGet]
-        public List<FiguresDTO> GetSearchResultsBy(string filter)
+        public List<FiguresDTO> GetSearchResultsBy(string filter , string searchRole, string isFullSearch, string searchCount)
         {
-            
-            var results = cugDB.Mains.Where(m => m.Title.Contains(filter)  || 
-                                                m.Meaning.Contains(filter) //||
-                                                //m.Body.Contains(filter)
-                                                ).Select(m => new FiguresDTO
+
+            var results = new List<FiguresDTO>();
+
+            switch (searchRole)
             {
-                Id = m.Id,
-                Idx = m.Idx,
-                Title = m.Title,
-                Meaning = m.Meaning//,
-               // Body = (m.Body.Length >100) ? m.Body.Substring(0,100) : m.Body
-            }).ToList();
+                case "ALL":
+
+                    if (isFullSearch == "false" && (searchCount == "1"))
+                        results = cugDB.Mains.Where(m => m.Title.Contains(filter) ||
+                                                   m.Meaning.Contains(filter)  //||
+                                                                               //  m.Body.Contains(filter)
+                                                   ).Select(m => new FiguresDTO
+                                                   {
+                                                       Id = m.Id,
+                                                       Idx = m.Idx,
+                                                       Title = m.Title,
+                                                       Meaning = m.Meaning
+                                                   }).ToList();
+                    else if((isFullSearch == "true") && (searchCount == "2")) //get body search
+                        results = cugDB.Mains.Where(m =>   m.Body.Contains(filter)
+                                                && !m.Title.Contains(filter)
+                                                && !m.Meaning.Contains(filter)
+                                               ).Select(m => new FiguresDTO
+                                               {
+                                                   Id = m.Id,
+                                                   Idx = m.Idx,
+                                                   Title = m.Title,
+                                                   Meaning = m.Meaning
+                                               }).Take(30).ToList();                   
+
+                    break;
+
+                case "Radiation":
+                    results = cugDB.Mains.Where(m => m.Title.Contains(filter) ||
+                                                m.Meaning.Contains(filter) &&
+                                                m.CategoryN.Equals(searchRole)
+                                                ).Select(m => new FiguresDTO
+                                                {
+                                                    Id = m.Id,
+                                                    Idx = m.Idx,
+                                                    Title = m.Title,
+                                                    Meaning = m.Meaning
+                                                }).ToList();
+                    break;
+                case "Aqua":
+                    results = cugDB.Mains.Where(m => m.Title.Contains(filter) ||
+                                                m.Meaning.Contains(filter) &&
+                                                m.CategoryN.Equals(searchRole)
+                                                ).Select(m => new FiguresDTO
+                                                {
+                                                    Id = m.Id,
+                                                    Idx = m.Idx,
+                                                    Title = m.Title,
+                                                    Meaning = m.Meaning
+                                                }).ToList();
+                    break;
+                case "Biblopedia":
+                    results = cugDB.Mains.Where(m => m.Title.Contains(filter) ||
+                                                m.Meaning.Contains(filter) &&
+                                                m.CategoryN.Equals(searchRole)
+                                                ).Select(m => new FiguresDTO
+                                                {
+                                                    Id = m.Id,
+                                                    Idx = m.Idx,
+                                                    Title = m.Title,
+                                                    Meaning = m.Meaning
+                                                }).ToList();
+                    break;
+                default:
+                    break;
+            }
 
             if (results != null) return results;
             return null;
